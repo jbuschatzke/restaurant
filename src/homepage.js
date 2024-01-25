252e8380a123b5e0b9b9dd9b4c565ac08d50eb8a
Joey <joezzles@gmail.com>
2024-01-25 12:41:15 -0800

    Initialize webpack, file structure, and js rendering of webpage

---
+++ src/homepage.js
@@ -0,0 +1,12 @@
+export default function createPage() {
+    const homeContainer = document.getElementById('content');
+
+    const homeHeader = document.createElement('h1');
+    homeHeader.textContent = "Welcome to Five Nights At Luigi's!";
+    homeContainer.appendChild(homeHeader);
+
+    const homeFiller = document.createElement('p');
+    homeFiller.textContent = 
+        "Spend five wonderful nights with us at Luigi's and experience a dining adventure the likes of which has never been seen before. With incredible dishes, exciting mysteries, and a killer chef, you'll never know what to expect next, but one things for certain: your taste buds will be delighted at every turn!";
+    homeContainer.appendChild(homeFiller);
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log("Hello World!");
+
+import createPage from "./homepage";
+
+createPage();
+
+const homeDiner = document.createElement("img");
+homeDiner.src = "./tables.jpg";
+document.getElementById('content').appendChild(homeDiner);
